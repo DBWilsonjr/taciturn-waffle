e15c647b8553399e758666880a5afb82e8ff0697
DB Wilson jr <dbwilsonjr@gmail.com>
2015-10-27 01:31:56 -0400

    Initial

---
+++ CPT185-Test-2-Revamp/Form1.Designer.cs
@@ -0,0 +1,377 @@
+﻿namespace CPT185_Test_2_Revamp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.txtLrg1 = new System.Windows.Forms.TextBox();
+            this.txtLrg2 = new System.Windows.Forms.TextBox();
+            this.txtLrg3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.grbLarge = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.grbSmall = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtSml3 = new System.Windows.Forms.TextBox();
+            this.txtSml1 = new System.Windows.Forms.TextBox();
+            this.txtSml2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.grbContain = new System.Windows.Forms.GroupBox();
+            this.grbLarge.SuspendLayout();
+            this.grbSmall.SuspendLayout();
+            this.grbContain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcess.Location = new System.Drawing.Point(28, 145);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(186, 43);
+            this.btnProcess.TabIndex = 0;
+            this.btnProcess.Text = "&Check out these fish!";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(28, 326);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(186, 43);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.Location = new System.Drawing.Point(100, 16);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(52, 17);
+            this.lblOutput.TabIndex = 2;
+            this.lblOutput.Text = "label1";
+            // 
+            // txtLrg1
+            // 
+            this.txtLrg1.Location = new System.Drawing.Point(91, 23);
+            this.txtLrg1.Name = "txtLrg1";
+            this.txtLrg1.Size = new System.Drawing.Size(49, 23);
+            this.txtLrg1.TabIndex = 3;
+            this.txtLrg1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtLrg2
+            // 
+            this.txtLrg2.Location = new System.Drawing.Point(91, 67);
+            this.txtLrg2.Name = "txtLrg2";
+            this.txtLrg2.Size = new System.Drawing.Size(49, 23);
+            this.txtLrg2.TabIndex = 4;
+            this.txtLrg2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtLrg3
+            // 
+            this.txtLrg3.Location = new System.Drawing.Point(91, 111);
+            this.txtLrg3.Name = "txtLrg3";
+            this.txtLrg3.Size = new System.Drawing.Size(49, 23);
+            this.txtLrg3.TabIndex = 5;
+            this.txtLrg3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Fish #1";
+            // 
+            // grbLarge
+            // 
+            this.grbLarge.Controls.Add(this.label10);
+            this.grbLarge.Controls.Add(this.label9);
+            this.grbLarge.Controls.Add(this.label8);
+            this.grbLarge.Controls.Add(this.label3);
+            this.grbLarge.Controls.Add(this.label2);
+            this.grbLarge.Controls.Add(this.label1);
+            this.grbLarge.Controls.Add(this.txtLrg3);
+            this.grbLarge.Controls.Add(this.txtLrg1);
+            this.grbLarge.Controls.Add(this.txtLrg2);
+            this.grbLarge.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grbLarge.Location = new System.Drawing.Point(12, 80);
+            this.grbLarge.Name = "grbLarge";
+            this.grbLarge.Size = new System.Drawing.Size(212, 152);
+            this.grbLarge.TabIndex = 7;
+            this.grbLarge.TabStop = false;
+            this.grbLarge.Text = "Largest Fish (lbs)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Fish #2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Fish #3";
+            // 
+            // grbSmall
+            // 
+            this.grbSmall.Controls.Add(this.label13);
+            this.grbSmall.Controls.Add(this.label12);
+            this.grbSmall.Controls.Add(this.label11);
+            this.grbSmall.Controls.Add(this.label4);
+            this.grbSmall.Controls.Add(this.label5);
+            this.grbSmall.Controls.Add(this.label6);
+            this.grbSmall.Controls.Add(this.txtSml3);
+            this.grbSmall.Controls.Add(this.txtSml1);
+            this.grbSmall.Controls.Add(this.txtSml2);
+            this.grbSmall.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grbSmall.Location = new System.Drawing.Point(257, 80);
+            this.grbSmall.Name = "grbSmall";
+            this.grbSmall.Size = new System.Drawing.Size(212, 152);
+            this.grbSmall.TabIndex = 9;
+            this.grbSmall.TabStop = false;
+            this.grbSmall.Text = "Smallest Fish (lbs)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Fish #3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Fish #2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 17);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Fish #1";
+            // 
+            // txtSml3
+            // 
+            this.txtSml3.Location = new System.Drawing.Point(91, 111);
+            this.txtSml3.Name = "txtSml3";
+            this.txtSml3.Size = new System.Drawing.Size(49, 23);
+            this.txtSml3.TabIndex = 5;
+            this.txtSml3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtSml1
+            // 
+            this.txtSml1.Location = new System.Drawing.Point(91, 23);
+            this.txtSml1.Name = "txtSml1";
+            this.txtSml1.Size = new System.Drawing.Size(49, 23);
+            this.txtSml1.TabIndex = 3;
+            this.txtSml1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtSml2
+            // 
+            this.txtSml2.Location = new System.Drawing.Point(91, 67);
+            this.txtSml2.Name = "txtSml2";
+            this.txtSml2.Size = new System.Drawing.Size(49, 23);
+            this.txtSml2.TabIndex = 4;
+            this.txtSml2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label7.Location = new System.Drawing.Point(25, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(686, 124);
+            this.label7.TabIndex = 10;
+            this.label7.Text = resources.GetString("label7.Text");
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(146, 26);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(34, 17);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "lbs.";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(146, 70);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(34, 17);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "lbs.";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(146, 114);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(34, 17);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "lbs.";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(146, 26);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(34, 17);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "lbs.";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(146, 70);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(34, 17);
+            this.label12.TabIndex = 13;
+            this.label12.Text = "lbs.";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(146, 114);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(34, 17);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "lbs.";
+            // 
+            // grbContain
+            // 
+            this.grbContain.Controls.Add(this.lblOutput);
+            this.grbContain.Controls.Add(this.grbLarge);
+            this.grbContain.Controls.Add(this.grbSmall);
+            this.grbContain.Location = new System.Drawing.Point(233, 129);
+            this.grbContain.Name = "grbContain";
+            this.grbContain.Size = new System.Drawing.Size(478, 240);
+            this.grbContain.TabIndex = 11;
+            this.grbContain.TabStop = false;
+            this.grbContain.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnProcess;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(723, 385);
+            this.Controls.Add(this.grbContain);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnProcess);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fish Processing App";
+            this.grbLarge.ResumeLayout(false);
+            this.grbLarge.PerformLayout();
+            this.grbSmall.ResumeLayout(false);
+            this.grbSmall.PerformLayout();
+            this.grbContain.ResumeLayout(false);
+            this.grbContain.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.TextBox txtLrg1;
+        private System.Windows.Forms.TextBox txtLrg2;
+        private System.Windows.Forms.TextBox txtLrg3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox grbLarge;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox grbSmall;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtSml3;
+        private System.Windows.Forms.TextBox txtSml1;
+        private System.Windows.Forms.TextBox txtSml2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.GroupBox grbContain;
+    }
+}
+
---
+++ CPT185-Test-2-Revamp/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CPT185_Test_2_Revamp
+{
+    public partial class Form1 : Form
+    {
+        Boolean valid = true; //This is a 'global' variable, though if we were working with multipe forms it couldn't
+        //actually be called from this form. This is just me being nit-picky since we're only using this form.
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            double[] fishRet = new double[8]; //Local variable where returns from our method will be stored
+            string statMsg = ""; //Local variable that will hold what we what to output in pretty text
+
+            /*Our method call, which will compute data from our file and do all the math we need done.
+             *Methods are very fun, but it can take a bit to understand them. The default setting for a method is 'in', so if
+             *we called the method as Method(fishRet[0], fishRet[1], ...), it would be sending those values to the method.
+             *We don't actully need to send anything to the method, we only need information from it.
+             *Our method is defined with 'out' variables, so when we call the method we need to specify where the output is
+             *going and make sure we have as many 'out's in the call as we do in the defined method. The 'out' portion of
+             *the method call goes to local variable(s) and doesn't have to match the names declared in the method.
+             *Since this uses an array, you have to mentally assign what each element is; for this case, element-0
+             *is the total of our numbers, e-1 is the total number of records in our file, e's 2 - 4 are the biggest
+             *fish, and e's 5 - 7 are the smallest. If we were using single variables, we could just call them things like
+             *total, count, big1, big2, etc.; that would be easier to read through, but it uses more variables and arrays
+             *are part of the current topic (and it never hurts to practice those anyway).
+             */
+            SuperComputerator(out fishRet[0], out fishRet[1], out fishRet[2], out fishRet[3], out fishRet[4],
+                out fishRet[5], out fishRet[6], out fishRet[7]);
+
+            if (valid == true) //Reference to a global boolean variable that is set false if the method fails
+            {
+                statMsg = "The weight of all caught fish: " + fishRet[0].ToString() + "lbs.\n";
+                statMsg += "The total fish caught: " + fishRet[1] + "lbs.\n";
+                statMsg += "The average weight of fish: " + Math.Round(fishRet[0] / fishRet[1],2).ToString() + "\n";
+                lblOutput.Text = statMsg;
+                txtLrg1.Text = fishRet[2].ToString();
+                txtLrg2.Text = fishRet[3].ToString();
+                txtLrg3.Text = fishRet[4].ToString();
+                txtSml1.Text = fishRet[5].ToString();
+                txtSml2.Text = fishRet[6].ToString();
+                txtSml3.Text = fishRet[7].ToString();
+                grbContain.Visible = true;
+                
+            }
+            else
+                MessageBox.Show("Make sure your input file is located with the program and named flyingfish.txt", "File Read Error"); 
+            //The first quoted text is what is show in the box, the second quoted text is shown in the titlebar of the
+            //message box. Useful to help categorize messageboxes if you want to use more than one type in an else/catch.
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void SuperComputerator(out double total, out double cnt, out double large, out double large2, out double large3, out double small, out double small2, out double small3)
+        //Since this is returning to an array, all declared outs must be the same type (doubles in this case)
+        //Another note about 'out's from a method; they obviously go out in the order they're declared. In this case, total
+        //will go out first no matter where it is a dealt with in the actual method. This is important to keep in mind since
+        //we're working with an array and have to keep track of these things ourselves. Like any other time, declare in a way
+        //that makes it easiest to work with.
+        {
+            //These have to be set so the method doesn't break. If the try/catch is removed, the code below works with
+            //the code below, but be sure to uncomment line 68; the method needs a starting point for the variables and
+            //that isn't set when you declare them ("a ref or out parameter can't have a default value").
+            total = cnt = large = large2 = large3 = small = small2 = small3 = 0;
+            try
+            {
+                double[] fishies = new double[25];
+                int idx = 0;
+                //total = 0;
+
+                StreamReader infile1;
+                infile1 = File.OpenText("flyingfish.txt");
+
+                //After this loop, we will have the final amounts for total and cnt, Elements 0 and 1 in our array above
+                while (idx < fishies.Length && !infile1.EndOfStream)
+                {
+                    fishies[idx] = double.Parse(infile1.ReadLine());
+                    total += fishies[idx];
+                    //if (large < fishies[idx])
+                        //large = fishies[idx]
+                    //if (small == 0)
+                        //small = fishies[idx]
+                    //else if (small > fishies[idx])
+                        //small = fishies[idx]
+                    idx++;
+                }
+                infile1.Close();
+                cnt = idx;
+
+                //Gives large and small a starting value, the first from our file. We need to do this mostly for the small
+                //variables because they're currently set to 0, which is smaller than any fish in the file
+                large = large2 = large3 = small = small2 = small3 = fishies[0];
+
+                /*These loops will get us the numbers for large and small fish, or elements 2 - 4 and 5 - 7 in our
+                 *fishRet array above. If we were only getting the biggest and smallest fish, this could be worked into the loop
+                 *above, but even so it's easier to comprehend this way.
+                 *We're doing a top 3, just to do something different. This is much harder (impossible?) to do in a single
+                 *loop (and honestly the whole thing is better suited to its own method, but this is what happens when I 
+                 *change my mind mid-coding.
+                */
+
+                //The first loop will get the largest and smallest fish.
+                for (int i = 1; i < fishies.Length; i++) //i is set to 1 because we don't need to read the zero element again
+                {                           
+                    if (large < fishies[i])
+                        large = fishies[i];
+                    if (small > fishies[i])
+                        small = fishies[i]; 
+                }
+
+                //This time we do the same, but for the 2nd biggest (e-3) and smallest (e-6) This means we need an added
+                //check to avoid taking the same number as our largest and smallest fish from above
+                for (int i = 1; i < fishies.Length; i++) 
+                {                                        
+                    if (large2 < fishies[i] && fishies[i] < large) 
+                        large2 = fishies[i];
+                    if (small2 > fishies[i] && fishies[i] > small) 
+                        small2 = fishies[i]; 
+                }
+
+                //Same as previous, plus one more condition. I have little doubt there's a better way to do all of this
+                //Since this is all based on classwork, I'm not going to look into that. Keeping to known material for now.
+                for (int i = 1; i < fishies.Length; i++) 
+                {
+                    if (large3 < fishies[i] && fishies[i] < large && fishies[i] < large2)
+                        large3 = fishies[i];
+                    if (small3 > fishies[i] && fishies[i] > small && fishies[i] > small2)
+                        small3 = fishies[i];
+                }
+            }
+            catch
+            {
+                valid = false; //If something breaks, this will stop everything after the method call from happening
+            }
+        }
+    }
+}
